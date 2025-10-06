5c9ba84cc2f6d7d72c52c7ed96f017821a3b7825
GALANIS Georgios <ggalanis@netcompany.com>
2025-10-06 17:54:15 +0300

    remove key

--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -1,7 +1,9 @@
 import QuestionTimer from "./QuestionTime";
 import Answers from "./Answers";
 
-export default function Question({key, questionText, answers, onSelect, selectedAnswer, answerState, onSkipAnswer}){
+export default function Question({questionText, answers, onSelect, selectedAnswer, answerState, onSkipAnswer}){
+
+
     return <div id="question">
             <QuestionTimer timeout={10000} onTimeout={onSkipAnswer}/>
             <h2>{questionText}</h2>
