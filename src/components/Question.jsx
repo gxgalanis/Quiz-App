7873973ead7508a9f233ef3f4f8261fb83ba0659
GALANIS Georgios <ggalanis@netcompany.com>
2025-10-06 18:12:47 +0300

    changes to work on later

--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -1,16 +1,44 @@
 import QuestionTimer from "./QuestionTime";
 import Answers from "./Answers";
+import QUESTIONS from '../questions.js';
+import { useState } from "react";
 
-export default function Question({questionText, answers, onSelect, selectedAnswer, answerState, onSkipAnswer}){
+export default function Question({questionIndex, onSelectAnswer, onSkipAnswer}){
+    const [answer,setAnswer] = useState({
+        selectedAnswer: '',
+        isCorrect: null
+    });
 
+    function handleSelectAnswer(answer){
+        setAnswer({
+            selectedAnswer: answer,
+            isCorrect: null
+        });
+        
+        setTimeout(()=>{
+            setAnswer({
+                selectedAnswer: answer,
+                isCorrect: QUESTIONS[questionIndex].answers[0] === answer
+            });
+            
+            setTimeout(()=>{
+                onSelectAnswer(answer);
+            },2000);
+        },1000);
+    }
+
+    let answerState  = '';
+    if(answer.selectedAnswer){
+        answerState = answerState.isCorrect ? 'correct' : 'wrong';
+    }
 
     return <div id="question">
             <QuestionTimer timeout={10000} onTimeout={onSkipAnswer}/>
-            <h2>{questionText}</h2>
+            <h2>{QUESTIONS[questionIndex].text}</h2>
             <Answers
-                answers={answers} 
-                selectedAnswer={selectedAnswer} 
+                answers={QUESTIONS[questionIndex].answers} 
+                selectedAnswer={answer.selectedAnswer} 
                 answerState={answerState}
-                onSelect={onSelect}/>
+                onSelect={handleSelectAnswer}/>
         </div>;
 }
