4775b28f266a40e95f23fe40ec9284a18c1dfd37
Georgios Galanis <gxgalanis@gmail.com>
2025-10-07 23:29:24 +0300

    disable buttons when answered is picked

--- src/components/Answers.jsx
+++ src/components/Answers.jsx
@@ -28,7 +28,11 @@ export default function Answers({
 
         return (
           <li key={answer} className="answer">
-            <button onClick={() => onSelect(answer)} className={cssClasses}>
+            <button
+              onClick={() => onSelect(answer)}
+              className={cssClasses}
+              disabled={answerState !== ""}
+            >
               {answer}
             </button>
           </li>
